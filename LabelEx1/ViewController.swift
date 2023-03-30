d614013ee32320481f9d6de62fe0c5349e3e94cd
PrathamSutar <prathameshsutar@nimapinfotech.com>
2023-03-30 12:13:22 +0530

    Commit

--- LabelEx1/ViewController.swift
+++ LabelEx1/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let lbl = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(lbl)
+        lbl.frame = CGRect.init(x: 40, y: 150, width: 300, height: 45)
+        lbl.backgroundColor = .cyan
+        lbl.text = "Hi...Shubham"
+        lbl.textAlignment = .center
+        lbl.textColor = .black
     }
 
 
